c48d7e93e9371f0ee324bd0ebe526d30a0a978b1
KbTech17 <info.kbtech17@gmail.com>
2024-09-07 13:10:11 +0545

    Jitpack

--- DPermission/build.gradle.kts
+++ DPermission/build.gradle.kts
@@ -1,5 +1,3 @@
-//import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -8,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.dp_th.dpermission"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
@@ -48,38 +46,3 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.appcompat)
 }
-
-//mavenPublishing {
-//    publishToMavenCentral(SonatypeHost.S01)
-//    signAllPublications()
-//}
-
-//mavenPublishing {
-//    coordinates("com.dp_th.dpermission", "dpermission", "2.1.1")
-//
-//    pom {
-//        name.set("DPermission")
-//        description.set("DPermission is an open source Android library that makes handling runtime permissions extremely easy and supports newly devices too.")
-//        inceptionYear.set("2024")
-//        url.set("https://github.com/KbTech17/DPermission/")
-//        licenses {
-//            license {
-//                name.set("The Apache License, Version 2.0")
-//                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//            }
-//        }
-//        developers {
-//            developer {
-//                id.set("KbTech17")
-//                name.set("Kb Tech")
-//                url.set("https://github.com/KbTech17/")
-//            }
-//        }
-//        scm {
-//            url.set("https://github.com/KbTech17/DPermission/")
-//            connection.set("scm:git:git://github.com/KbTech17/DPermission.git")
-//            developerConnection.set("scm:git:ssh://git@github.com/KbTech17/DPermission.git")
-//        }
-//    }
-//}
