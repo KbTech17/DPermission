0f864a41e2cf2e54dc1656176250dab54e019e2b
KbTech17 <info.kbtech17@gmail.com>
2024-09-07 11:17:50 +0545

    FirstCommit

--- app/src/main/java/com/dp_th/dpermission/MainActivity.java
+++ app/src/main/java/com/dp_th/dpermission/MainActivity.java
@@ -1,10 +1,26 @@
 package com.dp_th.dpermission;
 
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission_group.CAMERA;
+
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.dp_th.dpermission.callback.ExplainReasonCallback;
+import com.dp_th.dpermission.callback.ForwardToSettingsCallback;
+import com.dp_th.dpermission.callback.RequestCallback;
+
+import java.util.List;
+
+import com.dp_th.dpermission.R;
+import com.dp_th.dpermission.request.ExplainScope;
+import com.dp_th.dpermission.request.ForwardScope;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +29,83 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        findViewById(R.id.btnAsk).setOnClickListener(view -> askPerm1());
+        findViewById(R.id.btnAsk2).setOnClickListener(view -> askPerm2());
+        findViewById(R.id.btnAsk3).setOnClickListener(view -> askPerm3());
+        findViewById(R.id.btnAsk4).setOnClickListener(view -> askPerm4());
     }
+
+    private void askPerm1() {
+        DPermission.with(this)
+                .permissions(READ_CONTACTS, CAMERA, CALL_PHONE)
+                .request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
+                        if (allGranted) {
+                            Toast.makeText(MainActivity.this, "All permissions are granted", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "These permissions are denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void askPerm2() {
+        DPermission.with(this)
+                .permissions(READ_CONTACTS, CAMERA, CALL_PHONE)
+                .onExplainRequestReason(new ExplainReasonCallback() {
+                    @Override
+                    public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList) {
+                        scope.showRequestReasonDialog(deniedList, "These permissions are required!", "OK", "Cancel");
+                    }
+                })
+                .request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
+                        if (allGranted) {
+                            Toast.makeText(MainActivity.this, "All permissions are granted", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "These permissions are denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void askPerm3() {
+        DPermission.with(this)
+                .permissions(READ_CONTACTS, CAMERA, CALL_PHONE)
+                .onExplainRequestReason(new ExplainReasonCallback() {
+                    @Override
+                    public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList) {
+                        scope.showRequestReasonDialog(deniedList, "These permissions are required!", "OK", "Cancel");
+                    }
+                })
+                .onForwardToSettings(new ForwardToSettingsCallback() {
+                    @Override
+                    public void onForwardToSettings(@NonNull ForwardScope scope, @NonNull List<String> deniedList) {
+                        scope.showForwardToSettingsDialog(deniedList, "You need to allow necessary permissions in Settings manually", "OK", "Cancel");
+                    }
+                })
+                .request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
+                        if (allGranted) {
+                            Toast.makeText(MainActivity.this, "All permissions are granted", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "These permissions are denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
+    private void askPerm4() {
+        DPermission.with(this)
+                .permissions(READ_CONTACTS, CAMERA, CALL_PHONE)
+                .explainReasonBeforeRequest()
+//        ..................YOUR_CODE......................
+        ;
+    }
+
 }
