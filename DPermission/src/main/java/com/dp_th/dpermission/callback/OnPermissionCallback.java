f1a81463ad6441607ed19ca122a067b3b02a2081
KbTech17 <info.kbtech17@gmail.com>
2024-09-07 18:06:53 +0545

    Initial Release

--- DPermission/src/main/java/com/dp_th/dpermission/callback/OnPermissionCallback.java
+++
@@ -1,9 +0,0 @@
-package com.dp_th.dpermission.callback;
-
-import androidx.annotation.NonNull;
-
-import java.util.List;
-
-public interface OnPermissionCallback {
-    void onPermissionResult(boolean isGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList);
-}
